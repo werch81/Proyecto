a78dea22d5a09edb061c0172f3a8128662fbbc6d
WERCH81 <werch81@gmail.com>
2023-03-28 15:22:06 -0500

    primera actualizacion

--- prueba.py
+++ prueba.py
@@ -1,4 +1,6 @@
+# suma de 2 numeros 
+
 a = input("Digite el primer numero y oprima enter ")
-b = input("Digite el segundo numero y oprima enter ")
+b = input("Digite el segundo numero y oprima enter  ")
 c = float(a) + float(b)
 print ("La suma es :", c)
